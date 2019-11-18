5d43d07385e84218328f289e0956fa8babfc7a34
geneli <p_gebejli@tencent.com>
2019-11-18 19:46:06 +0800

    slinding window max

--- src/main/java/chapter1/StackAndQueue7.java
+++ src/main/java/chapter1/StackAndQueue7.java
@@ -16,28 +16,33 @@ class SlidingWindowMaximum {
      */
     @SuppressWarnings("all")
     public static int[] getMaxArray(int[] arr, int ws) {
+        //结果
         int[] result = new int[arr.length - ws + 1];
+        //result 数组指针
         int index = 0;
+        //双端队列
         Deque<Integer> maxDeque = new LinkedList<>();
         for (int i = 0; i < arr.length; i++) {
-
+            //保证本次队首为最大值
             while (!maxDeque.isEmpty() && arr[maxDeque.peekLast()] < arr[i]) {
                 maxDeque.pollLast();
             }
-
             maxDeque.addLast(i);
 
-
+            //判断队首值是否在窗口内，不在窗口内则移除
             if (maxDeque.peekFirst() == i - ws) {
                 maxDeque.pollFirst();
             }
 
+            //满足窗口条件存入数组
             if (i >= ws - 1) {
                 result[index++] = arr[maxDeque.peekFirst()];
             }
+
         }
         return result;
     }
+
 
 
 }
